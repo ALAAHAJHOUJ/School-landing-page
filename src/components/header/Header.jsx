0326b6be54873cf75bb90ef65c6ee9d192bcb722
hajhouj <alaa.spread@gmail.com>
2026-01-01 23:27:19 +0100

    responsive finis

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import './Header.css'
 import logo from'../../assets/logo.png'
-
+import menuIcon from '../../assets/menu-icon.png'
 
 
 function Header() {
   const [sticky,setStciky]=useState(false)
-
+  const [cacher,setCacher]=useState(true)
 
   useEffect(()=>{
     window.addEventListener("scroll",(e)=>{
        window.scrollY>500?setStciky(true):setStciky(false)
     })
   },[])
+
+
+  const changerVisibilte=()=>{
+      setCacher((prev)=>{return !prev})
+  }
+
+
   return (
     <>
         <nav className={`nav1 container1 ${sticky?'nav2':''}`}>
               <img src={logo} alt="image de projet" className='image' />
-              <ul className='elements'>
+              <img src={menuIcon} alt='image de projet' className='image2' onClick={()=>{changerVisibilte()}}></img>
+              <ul className={`elements ${cacher?'':'visible'}`}>
                     <li>Home</li>
                     <li>Program</li>
                     <li>About Us</li>
